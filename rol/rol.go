e021d00ac0251f9768ee93e9e5729c971ae5dac6
mibori <miborishante@gmail.com>
2015-07-15 21:46:03 +0300

    exact search draft

--- rol/rol.go
+++ rol/rol.go
@@ -5,9 +5,33 @@ package rol
 #include <libryftone.h>
 */
 import "C"
+import "unsafe"
 
 type RolDS struct {
-	cds C.rol_data_set_t
+	cds      C.rol_data_set_t
+	cStrings []*C.Char
+}
+
+func freeAllCStrings(cStrings []*C.Char) {
+	for p := range cStrings {
+		C.free(unsafe.Pointer(p))
+	}
+}
+
+func (ds *RolDS) registerCString(cString *C.Char) {
+	if cString != nil {
+		ds.cStrings = append(ds.cStrings, cString)
+	}
+}
+
+func (ds *RolDS) freeAllCStrings() {
+	freeAllCStrings(ds.cStrings)
+}
+
+func rolDSFromCds(cds C.rol_data_set_t) {
+	ds := new(RolDS)
+	ds.cds = cds
+	return ds
 }
 
 func RolDSCreate() *RolDS {
@@ -23,19 +47,48 @@ func RolDSCreateNodes(nodesCount uint8) *RolDS {
 }
 
 func (ds *RolDS) AddFile(name string) bool {
-	return false
+	var cFilename *C.char = C.CString(name)
+	ds.registerCString(cFilename)
+	return bool(C.rol_ds_add_file(cFilename))
 }
 
-func (ds *RolDS) Delete() { // https://golang.org/pkg/runtime/#SetFinalizer
+func (ds *RolDS) Delete() { //TODO: https://golang.org/pkg/runtime/#SetFinalizer
+	C.rol_ds_delete(&ds.cds)
+	ds.freeAllCStrings()
 }
 
 func (ds *RolDS) SearchExact(
 	resultsFile, query string,
 	surroundingWidth uint16,
-	delimeter, indexResultsFile string,
-	percentageCallback func() uint8,
+	delimeter string,
+	indexResultsFile *string,
 ) *RolDS {
-	return nil
+	var (
+		cResultsFile      *C.Char = C.CString(resultFile)
+		cQuery            *C.Char = C.CString(query)
+		cDelimeter        *C.Char = C.CString(delimeter)
+		cIndexResultsFile *C.Char = nil
+	)
+
+	if indexResultsFile != nil {
+		cIndexResultsFile = C.CString(indexResultsFile)
+	}
+
+	defer freeAllCStrings([]*C.Char{cResultsFile, cQuery, cDelimeter, cIndexResultsFile})
+
+	var newCds C.rol_data_set_t = C.rol_ds_search_exact(
+		ds.cds,
+		cResultsFile,
+		cQuery,
+		C.uint16_t(surroundingWidth),
+		cDelimeter,
+		cIndexResultsFile,
+		nil,
+	)
+
+	cds := rolDSFromCds(newCds)
+
+	return cds
 }
 
 func (ds *RolDS) SearchFuzzyHamming(
