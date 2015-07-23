a692be8de7b651c6e8036821303aa3764817dbd4
mibori <miborishante@gmail.com>
2015-07-23 19:26:43 +0300

    test modification 22

--- ryft-server/streaming.go
+++ ryft-server/streaming.go
@@ -4,7 +4,10 @@ import (
 	"fmt"
 	"io"
 	"log"
+	"net/http"
 	"os"
+	"strconv"
+	"strings"
 )
 
 // type ExactResult struct {
@@ -14,13 +17,46 @@ import (
 // 	Data   []byte `json:"data"`
 // }
 
-// type FuzzyResult struct {
-// 	File      string `json:"file"`
-// 	Offset    uint64 `json:"offset"`
-// 	Length    uint16 `json:"length"`
-// 	Fuzziness uint8  `json:"fuzziness"`
-// 	Data      []byte `json:"data"`
-// }
+type IdxRecord struct {
+	File      string `json:"file"`
+	Offset    uint64 `json:"offset"`
+	Length    uint16 `json:"length"`
+	Fuzziness uint8  `json:"fuzziness"`
+	Data      []byte `json:"data"`
+}
+
+func NewIdxRecord(line string) (r IdxRecord, err error) {
+	fields := strings.Split(text, ",")
+	if len(fields) < 4 {
+		panic(&ServerError{http.StatusInternalServerError,
+			fmt.Sprintf("Could not parse index file `%s`, string `%s`", idxFile.Name(), text)})
+	}
+
+	// NOTE: filename (first field of idx file) may contains ','
+	for len(fields) != 4 {
+		fields = append([]string{fields[0] + "," + fields[1]}, fields[2:]...)
+	}
+
+	r.File = fields[0]
+
+	if r.Offset, err = strconv.ParseUint(fields[1], 10, 64); err != nil {
+		return
+	}
+
+	var length uint64
+	if length, err = strconv.ParseUint(fields[2], 10, 16); err != nil {
+		return
+	}
+	r.Length = uint16(length)
+
+	var fuzziness uint64
+	if fuzziness, err = strconv.ParseUint(fields[3], 10, 8); err != nil {
+		return
+	}
+	r.Fuzziness = fuzziness
+
+	return
+}
 
 // func StreamJsonContentOfArray(resultsFile, idxFile *os.File, w io.Writer, isFuzzy bool) {
 // 	idxScanner := bufio.NewScanner(idxFile)
@@ -47,34 +83,34 @@ import (
 // 		text := idxScanner.Text()
 // 		log.Println("+ END IDX RECORD:", text)
 
-// 		fields := strings.Split(text, ",")
-// 		if len(fields) < 4 {
-// 			panic(&ServerError{http.StatusInternalServerError,
-// 				fmt.Sprintf("Could not parse index file `%s`, string `%s`", idxFile.Name(), text)})
-// 		}
+// fields := strings.Split(text, ",")
+// if len(fields) < 4 {
+// 	panic(&ServerError{http.StatusInternalServerError,
+// 		fmt.Sprintf("Could not parse index file `%s`, string `%s`", idxFile.Name(), text)})
+// }
 
-// 		// NOTE: filename (first field of idx file) may contains ','
-// 		for len(fields) != 4 {
-// 			fields = append([]string{fields[0] + "," + fields[1]}, fields[2:]...)
-// 		}
+// // NOTE: filename (first field of idx file) may contains ','
+// for len(fields) != 4 {
+// 	fields = append([]string{fields[0] + "," + fields[1]}, fields[2:]...)
+// }
 
 // 		var record interface{}
 
-// 		filename := fields[0]
+// filename := fields[0]
 
-// 		var offset uint64
-// 		if offset, err = strconv.ParseUint(fields[1], 10, 64); err != nil {
-// 			log.Printf("Parse int error: %s", err.Error())
-// 			panic(&ServerError{http.StatusInternalServerError, err.Error()})
-// 		}
+// var offset uint64
+// if offset, err = strconv.ParseUint(fields[1], 10, 64); err != nil {
+// 	log.Printf("Parse int error: %s", err.Error())
+// 	panic(&ServerError{http.StatusInternalServerError, err.Error()})
+// }
 
-// 		var length uint64
-// 		if length, err = strconv.ParseUint(fields[2], 10, 16); err != nil {
-// 			log.Printf("Parse int error: %s", err.Error())
-// 			panic(&ServerError{http.StatusInternalServerError, err.Error()})
-// 		}
+// var length uint64
+// if length, err = strconv.ParseUint(fields[2], 10, 16); err != nil {
+// 	log.Printf("Parse int error: %s", err.Error())
+// 	panic(&ServerError{http.StatusInternalServerError, err.Error()})
+// }
 
-// 		log.Printf("+ IDX PARSED: %s, %d, %d", filename, offset, length)
+// log.Printf("+ IDX PARSED: %s, %d, %d", filename, offset, length)
 
 // 		data := make([]byte, length)
 // 		n, err := io.ReadFull(resultsFile, data)
@@ -90,11 +126,11 @@ import (
 // 		}
 
 // 		if isFuzzy {
-// 			var fuzziness uint64
-// 			if fuzziness, err = strconv.ParseUint(fields[3], 10, 8); err != nil {
-// 				log.Printf("Parse int error: %s", err.Error())
-// 				panic(&ServerError{http.StatusInternalServerError, err.Error()})
-// 			}
+// var fuzziness uint64
+// if fuzziness, err = strconv.ParseUint(fields[3], 10, 8); err != nil {
+// 	log.Printf("Parse int error: %s", err.Error())
+// 	panic(&ServerError{http.StatusInternalServerError, err.Error()})
+// }
 // 			record = FuzzyResult{File: filename, Offset: offset, Length: uint16(length), Fuzziness: uint8(fuzziness), Data: data}
 // 		} else {
 // 			record = ExactResult{File: filename, Offset: offset, Length: uint16(length), Data: data}
@@ -118,23 +154,28 @@ import (
 // }
 
 func StreamJson(resultsFile, idxFile *os.File, w io.Writer, completion chan error) {
-	idxLines := make(chan string, 64)
+	//idxLines := make(chan string, 64)
+	idxRecords := make(chan IdxRecord, 64)
 	go func() {
 		//idxScanner := bufio.NewScanner(idxFile)
 		for {
 			select {
 			case <-completion:
-				linesScan(idxFile, idxLines)
-				close(idxLines)
+				// linesScan(idxFile, idxLines)
+				// close(idxLines)
+				recordsScan(idxFile, idxRecords)
 				return
 			default:
-				linesScan(idxFile, idxLines)
+				recordsScan(idxFile, idxRecords)
 			}
 		}
 	}()
 
-	for line := range idxLines {
-		log.Printf("+ SCAN-LINE:%s", line)
+	// for line := range idxLines {
+	// 	log.Printf("+ SCAN-LINE:%s", line)
+	// }
+	for r := range idxRecords {
+		log.Printf("+ RECORD %+v", r)
 	}
 }
 
@@ -149,17 +190,22 @@ func linesScan(r io.Reader, linesChan chan string) {
 	}
 }
 
-// func linesScan(scanner *bufio.Scanner, linesChan chan string) {
-// 	log.Println("+ lines scan")
-// 	for scanner.Scan() {
-// 		log.Println("+ line")
-// 		linesChan <- scanner.Text()
-// 	}
+func recordsScan(r io.Reader, recordsChan chan IdxRecord) {
+	for {
+		var line string
+		n, _ := fmt.Fscanln(r, &line)
+		if n == 0 {
+			break
+		}
 
-// 	if err := scanner.Err(); err != nil {
-// 		log.Fatalf("lineScan: %s", err.Error())
-// 	}
-// }
+		r, err := NewIdxRecord(line)
+		if err {
+			break
+		}
+
+		recordsChan <- r
+	}
+}
 
 /* Index file line format:
 
