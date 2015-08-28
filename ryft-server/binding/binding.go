fc08b4333bf201bf6dc650dc1dc0d4b59e9ccb40
Eugene Dubovik <edubovik90@gmail.com>
2015-08-28 11:59:41 -0400

    Binding fixes. Added UrlParsing and UrlQuery checker

--- ryft-server/binding/binding.go
+++ ryft-server/binding/binding.go
@@ -6,6 +6,7 @@ package binding
 import (
 	"fmt"
 	"log"
+	"strconv"
 
 	"github.com/gin-gonic/gin"
 )
@@ -17,24 +18,68 @@ type Search struct {
 	Fuzziness   uint8    `form:"fuzziness" json:"fuzziness"`                        // Is the fuzziness of the search. Measured as the maximum Hamming distance.
 }
 
+const (
+	queryTag       = "query"
+	filesTag       = "files"
+	surroundingTag = "surrounding"
+	fuzzinessTag   = "fuzziness"
+)
+
 func NewSearch(c *gin.Context) (*Search, error) {
 	s := new(Search)
-	if err := c.Bind(s); err != nil {
-		log.Printf("SEARCH = %+v", s)
-		if s.Query == "" {
-			return nil, fmt.Errorf("Query can not be empty")
-		}
-		if len(s.Files) <= 0 {
-			return nil, fmt.Errorf("At least one file should be specified")
-		}
-		if s.Surrounding <= 0 && s.Surrounding <= 32768 {
-			return nil, fmt.Errorf("Surrounding should not be less than 0 and not more than 32768")
-		}
-		if s.Fuzziness <= 0 && s.Fuzziness <= 255 {
-			return nil, fmt.Errorf("Fuzziness should not be less than 0 and not more than 255")
+
+	url := c.Request.URL.Query()
+
+	query, hasQuery := url[queryTag]
+	files, hasFiles := url[filesTag]
+	surrounding, hasSurrounding := url[surroundingTag]
+	fuzziness, hasFuzziness := url[fuzzinessTag]
+
+	if !hasQuery || len(query) <= 0 {
+		return nil, fmt.Errorf("Query can not be empty")
+	}
+
+	if !hasFiles || len(files) <= 0 {
+		return nil, fmt.Errorf("At least one file should be specified")
+	}
+
+	if !hasSurrounding {
+		s.Surrounding = 0
+	} else {
+		srValue, srValueErr := strconv.ParseInt(surrounding[0], 0, 64)
+
+		if srValueErr != nil {
+			return nil, fmt.Errorf("Could not parse Surrounding")
+		} else {
+			if srValue < 0 {
+				return nil, fmt.Errorf("Surrounding should not be less than 0")
+			}
+
+			if srValue > 32767 {
+				return nil, fmt.Errorf("Surrounding should not be more than 32767")
+			}
+
 		}
 	}
 
+	if !hasFuzziness {
+		s.Fuzziness = 0
+	} else {
+		fzValue, fzValueErr := strconv.ParseInt(fuzziness[0], 0, 64)
+		if fzValueErr != nil {
+			return nil, fmt.Errorf("Could not parse Fuzziness")
+		} else {
+			if fzValue < 0 {
+				return nil, fmt.Errorf("Fuzziness should not be less than 0")
+			}
+
+			if fzValue > 254 {
+				log.Println("inside")
+				return nil, fmt.Errorf("Fuzziness should not be more than 254 ")
+			}
+		}
+	}
+	c.Bind(s)
 	return s, nil
 
 }
