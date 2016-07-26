c0f5cc6a2db9199cbb3bfddc4e9f3082aacc1a59
Sergey Polichnoy <pilatuz@gmail.com>
2016-07-26 10:17:27 +0300

    WIP

--- test/decomp/decomp.go
+++ test/decomp/decomp.go
@@ -118,12 +118,9 @@ func (s *Scanner) Scan() (Token, string) {
 	} else if s.isLetter(r) {
 		s.unread()
 		return s.scanIdent()
-		//	} else if s.isDigit(r) {
-		//		s.unread()
-		//		return s.scanDigit()
-		//	} else if r == '"' {
-		//		s.unread()
-		//		return s.scanString()
+	} else if s.isDigit(r) {
+		s.unread()
+		return s.scanNumber()
 	}
 
 	// Otherwise read the individual character.
@@ -140,6 +137,11 @@ func (s *Scanner) Scan() (Token, string) {
 		return COMMA, string(r)
 	case '.':
 		return DOT, string(r)
+	case '"':
+		s.unread()
+		return s.scanString()
+	case '+', '-':
+		return s.scanNumber()
 	}
 
 	return ILLEGAL, string(r)
@@ -183,6 +185,135 @@ func (s *Scanner) scanIdent() (Token, string) {
 	}
 
 	return IDENT, buf.String()
+}
+
+// scanString consumes a contiguous string of non-quote characters.
+// Quote characters can be consumed if they're first escaped with a backslash.
+func (s *Scanner) scanString() (Token, string) {
+	var err error
+	//lit, err = ScanString(s.r)
+	
+
+// ScanString reads a quoted string from a rune reader.
+func ScanString(r io.RuneScanner) (string, error) {
+	ending, _, err := r.ReadRune()
+	if err != nil {
+		return "", errBadString
+	}
+
+	var buf bytes.Buffer
+	for {
+		ch0, _, err := r.ReadRune()
+		if ch0 == ending {
+			return buf.String(), nil
+		} else if err != nil || ch0 == '\n' {
+			return buf.String(), errBadString
+		} else if ch0 == '\\' {
+			// If the next character is an escape then write the escaped char.
+			// If it's not a valid escape then return an error.
+			ch1, _, _ := r.ReadRune()
+			if ch1 == 'n' {
+				_, _ = buf.WriteRune('\n')
+			} else if ch1 == '\\' {
+				_, _ = buf.WriteRune('\\')
+			} else if ch1 == '"' {
+				_, _ = buf.WriteRune('"')
+			} else if ch1 == '\'' {
+				_, _ = buf.WriteRune('\'')
+			} else {
+				return string(ch0) + string(ch1), errBadEscape
+			}
+		} else {
+			_, _ = buf.WriteRune(ch0)
+		}
+	}
+}
+	
+	
+	if err == errBadString {
+		return BADSTRING, pos, lit
+	} else if err == errBadEscape {
+		_, pos = s.r.curr()
+		return BADESCAPE, pos, lit
+	}
+	return STRING, pos, lit
+}
+
+// scanNumber consumes anything that looks like the start of a number.
+// Numbers start with a digit, full stop, plus sign or minus sign.
+// This function can return non-number tokens if a scan is a false positive.
+// For example, a minus sign followed by a letter will just return a minus sign.
+func (s *Scanner) scanNumber() (tok Token, pos Pos, lit string) {
+	var buf bytes.Buffer
+
+	// Check if the initial rune is a "+" or "-".
+	ch, pos := s.r.curr()
+	if ch == '+' || ch == '-' {
+		// Peek at the next two runes.
+		ch1, _ := s.r.read()
+		ch2, _ := s.r.read()
+		s.r.unread()
+		s.r.unread()
+
+		// This rune must be followed by a digit or a full stop and a digit.
+		if isDigit(ch1) || (ch1 == '.' && isDigit(ch2)) {
+			_, _ = buf.WriteRune(ch)
+		} else if ch == '+' {
+			return ADD, pos, ""
+		} else if ch == '-' {
+			return SUB, pos, ""
+		}
+	} else if ch == '.' {
+		// Peek and see if the next rune is a digit.
+		ch1, _ := s.r.read()
+		s.r.unread()
+		if !isDigit(ch1) {
+			return ILLEGAL, pos, "."
+		}
+
+		// Unread the full stop so we can read it later.
+		s.r.unread()
+	} else {
+		s.r.unread()
+	}
+
+	// Read as many digits as possible.
+	_, _ = buf.WriteString(s.scanDigits())
+
+	// If next code points are a full stop and digit then consume them.
+	isDecimal := false
+	if ch0, _ := s.r.read(); ch0 == '.' {
+		isDecimal = true
+		if ch1, _ := s.r.read(); isDigit(ch1) {
+			_, _ = buf.WriteRune(ch0)
+			_, _ = buf.WriteRune(ch1)
+			_, _ = buf.WriteString(s.scanDigits())
+		} else {
+			s.r.unread()
+		}
+	} else {
+		s.r.unread()
+	}
+
+	// Read as a duration or integer if it doesn't have a fractional part.
+	if !isDecimal {
+		// If the next rune is a duration unit (u,µ,ms,s) then return a duration token
+		if ch0, _ := s.r.read(); ch0 == 'u' || ch0 == 'µ' || ch0 == 's' || ch0 == 'h' || ch0 == 'd' || ch0 == 'w' {
+			_, _ = buf.WriteRune(ch0)
+			return DURATIONVAL, pos, buf.String()
+		} else if ch0 == 'm' {
+			_, _ = buf.WriteRune(ch0)
+			if ch1, _ := s.r.read(); ch1 == 's' {
+				_, _ = buf.WriteRune(ch1)
+			} else {
+				s.r.unread()
+			}
+			return DURATIONVAL, pos, buf.String()
+		}
+		s.r.unread()
+		return INTEGER, pos, buf.String()
+	}
+	return NUMBER, pos, buf.String()
 }
 
 // Parser represents a parser.
