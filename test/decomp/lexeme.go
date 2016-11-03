c8b206fed68cfb38dcff7fe46c2907605460185d
Sergey Polichnoy <pilatuz@gmail.com>
2016-11-03 14:18:18 +0300

    more lexeme tests

--- test/decomp/lexeme.go
+++ test/decomp/lexeme.go
@@ -27,95 +27,187 @@ func (lex Lexeme) String() string {
 
 // IsAnd checks is "AND" operator.
 func (lex Lexeme) IsAnd() bool {
-	return lex.token == IDENT && strings.EqualFold(lex.literal, "AND")
+	if lex.token != IDENT {
+		return false
+	}
+
+	return strings.EqualFold(lex.literal, "AND")
 }
 
 // IsXor checks "XOR" operator.
 func (lex Lexeme) IsXor() bool {
-	return lex.token == IDENT && strings.EqualFold(lex.literal, "XOR")
+	if lex.token != IDENT {
+		return false
+	}
+
+	return strings.EqualFold(lex.literal, "XOR")
 }
 
 // IsOr checks "OR" operator.
 func (lex Lexeme) IsOr() bool {
-	return lex.token == IDENT && strings.EqualFold(lex.literal, "OR")
+	if lex.token != IDENT {
+		return false
+	}
+
+	return strings.EqualFold(lex.literal, "OR")
 }
 
 // IsRawText checks "RAW_TEXT" input.
 func (lex Lexeme) IsRawText() bool {
-	return lex.token == IDENT && strings.EqualFold(lex.literal, "RAW_TEXT")
+	if lex.token != IDENT {
+		return false
+	}
+
+	return strings.EqualFold(lex.literal, "RAW_TEXT")
 }
 
 // IsRecord checks "RECORD" input.
 func (lex Lexeme) IsRecord() bool {
-	return lex.token == IDENT && strings.EqualFold(lex.literal, "RECORD")
+	if lex.token != IDENT {
+		return false
+	}
+
+	return strings.EqualFold(lex.literal, "RECORD")
 }
 
 // IsContains checks "CONTAINS" operator.
 func (lex Lexeme) IsContains() bool {
-	return lex.token == IDENT && strings.EqualFold(lex.literal, "CONTAINS")
+	if lex.token != IDENT {
+		return false
+	}
+
+	return strings.EqualFold(lex.literal, "CONTAINS")
 }
 
 // IsNotContains checks "NOT_CONTAINS" operator.
 func (lex Lexeme) IsNotContains() bool {
-	return lex.token == IDENT && strings.EqualFold(lex.literal, "NOT_CONTAINS")
+	if lex.token != IDENT {
+		return false
+	}
+
+	return strings.EqualFold(lex.literal, "NOT_CONTAINS")
 }
 
 // IsEquals checks "EQUALS" operator.
 func (lex Lexeme) IsEquals() bool {
-	return lex.token == IDENT && strings.EqualFold(lex.literal, "EQUALS")
+	if lex.token != IDENT {
+		return false
+	}
+
+	return strings.EqualFold(lex.literal, "EQUALS")
 }
 
 // IsNotEquals checks "NOT_EQUALS" operator.
 func (lex Lexeme) IsNotEquals() bool {
-	return lex.token == IDENT && strings.EqualFold(lex.literal, "NOT_EQUALS")
+	if lex.token != IDENT {
+		return false
+	}
+
+	return strings.EqualFold(lex.literal, "NOT_EQUALS")
 }
 
 // IsES checks "ES" search type.
 func (lex Lexeme) IsES() bool {
-	return lex.token == IDENT && (strings.EqualFold(lex.literal, "ES") || strings.EqualFold(lex.literal, "EXACT"))
+	if lex.token != IDENT {
+		return false
+	}
+
+	// a few aliases
+	return strings.EqualFold(lex.literal, "ES") ||
+		strings.EqualFold(lex.literal, "EXACT")
 }
 
 // IsFHS checks "FHS" search type.
 func (lex Lexeme) IsFHS() bool {
-	return lex.token == IDENT && (strings.EqualFold(lex.literal, "FHS") || strings.EqualFold(lex.literal, "HAMMING"))
+	if lex.token != IDENT {
+		return false
+	}
+
+	// a few aliases
+	return strings.EqualFold(lex.literal, "FHS") ||
+		strings.EqualFold(lex.literal, "HAMMING")
 }
 
 // IsFEDS checks "FEDS" search type.
 func (lex Lexeme) IsFEDS() bool {
-	return lex.token == IDENT && (strings.EqualFold(lex.literal, "FEDS") || strings.EqualFold(lex.literal, "EDIT"))
+	if lex.token != IDENT {
+		return false
+	}
+
+	// a few aliases
+	return strings.EqualFold(lex.literal, "FEDS") ||
+		strings.EqualFold(lex.literal, "EDIT") ||
+		strings.EqualFold(lex.literal, "EDIT_DIST") ||
+		strings.EqualFold(lex.literal, "EDIT_DISTANCE")
 }
 
 // IsDate checks "DATE" search type.
 func (lex Lexeme) IsDate() bool {
-	return lex.token == IDENT && strings.EqualFold(lex.literal, "DATE")
+	if lex.token != IDENT {
+		return false
+	}
+
+	return strings.EqualFold(lex.literal, "DATE")
 }
 
 // IsTime checks "TIME" search type.
 func (lex Lexeme) IsTime() bool {
-	return lex.token == IDENT && strings.EqualFold(lex.literal, "TIME")
+	if lex.token != IDENT {
+		return false
+	}
+
+	return strings.EqualFold(lex.literal, "TIME")
 }
 
 // IsNumber checks "NUMBER" search type.
 func (lex Lexeme) IsNumber() bool {
-	return lex.token == IDENT && (strings.EqualFold(lex.literal, "NUMBER") || strings.EqualFold(lex.literal, "NUMERIC"))
+	if lex.token != IDENT {
+		return false
+	}
+
+	// a few aliases
+	return strings.EqualFold(lex.literal, "NUMBER") ||
+		strings.EqualFold(lex.literal, "NUMERIC")
 }
 
 // IsCurrency checks "CURRENCY" search type.
 func (lex Lexeme) IsCurrency() bool {
-	return lex.token == IDENT && strings.EqualFold(lex.literal, "CURRENCY")
+	if lex.token != IDENT {
+		return false
+	}
+
+	// a few aliases
+	return strings.EqualFold(lex.literal, "CURRENCY") ||
+		strings.EqualFold(lex.literal, "MONEY")
 }
 
 // IsRegex checks "REGEX" search type.
 func (lex Lexeme) IsRegex() bool {
-	return lex.token == IDENT && (strings.EqualFold(lex.literal, "REGEX") || strings.EqualFold(lex.literal, "REGEXP") || strings.EqualFold(lex.literal, "REG_EXP") || strings.EqualFold(lex.literal, "RE"))
+	if lex.token != IDENT {
+		return false
+	}
+
+	// a few aliases
+	return strings.EqualFold(lex.literal, "REGEX") ||
+		strings.EqualFold(lex.literal, "REGEXP") ||
+		strings.EqualFold(lex.literal, "REG_EXP") ||
+		strings.EqualFold(lex.literal, "RE")
 }
 
 // IsIPv4 checks "IPv4" search type.
 func (lex Lexeme) IsIPv4() bool {
-	return lex.token == IDENT && strings.EqualFold(lex.literal, "IPv4")
+	if lex.token != IDENT {
+		return false
+	}
+
+	return strings.EqualFold(lex.literal, "IPv4")
 }
 
 // IsIPv6 checks "IPv6" search type.
 func (lex Lexeme) IsIPv6() bool {
-	return lex.token == IDENT && strings.EqualFold(lex.literal, "IPv6")
+	if lex.token != IDENT {
+		return false
+	}
+
+	return strings.EqualFold(lex.literal, "IPv6")
 }
