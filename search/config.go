eb77ed038b9578fca2727598a8e774eda32b81db
Sergey Polichnoy <pilatuz@gmail.com>
2017-02-14 12:58:53 +0300

    more transform unit tests

--- search/config.go
+++ search/config.go
@@ -54,6 +54,9 @@ type Config struct {
 	KeepIndexAs string
 	Delimiter   string
 
+	// post-processing transformations
+	Transforms []Transform
+
 	// processing control
 	ReportIndex bool // if false, no processing enabled at all (/count)
 	ReportData  bool // if false, just indexes will be read (format=null)
--- search/transform_test.go
+++ search/transform_test.go
@@ -6,8 +6,78 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-// test call script
-func TestCallScript(t *testing.T) {
+// test regexp-match transformation
+func TestRegexpMatch(t *testing.T) {
+	// good case
+	check := func(expr string, in string, expectedOut string, expectedSkip bool) {
+		tx, err := NewRegexpMatch(expr)
+		if !assert.NoError(t, err) {
+			return
+		}
+
+		out, skip, err := tx.Transform([]byte(in))
+		if assert.NotNil(t, out) && assert.NoError(t, err) {
+			assert.EqualValues(t, expectedOut, out)
+			assert.EqualValues(t, expectedSkip, skip)
+		}
+	}
+
+	// bad case
+	bad := func(expr string, in string, expectedError string) {
+		tx, err := NewRegexpMatch(expr)
+		if err != nil {
+			assert.Contains(t, err.Error(), expectedError)
+			return
+		}
+
+		out, _, err := tx.Transform([]byte(in))
+		if assert.Nil(t, out) && assert.Error(t, err) {
+			assert.Contains(t, err.Error(), expectedError)
+		}
+	}
+
+	check("$apple^", "hello", "hello", true)
+	check("^.*ell.*$", "hello", "hello", false)
+	bad("$(", "hello", "error parsing regexp")
+}
+
+// test regexp-replace transformation
+func TestRegexpReplace(t *testing.T) {
+	// good case
+	check := func(expr, templ string, in string, expectedOut string, expectedSkip bool) {
+		tx, err := NewRegexpReplace(expr, templ)
+		if !assert.NoError(t, err) {
+			return
+		}
+
+		out, skip, err := tx.Transform([]byte(in))
+		if assert.NotNil(t, out) && assert.NoError(t, err) {
+			assert.EqualValues(t, expectedOut, out)
+			assert.EqualValues(t, expectedSkip, skip)
+		}
+	}
+
+	// bad case
+	bad := func(expr, templ string, in string, expectedError string) {
+		tx, err := NewRegexpReplace(expr, templ)
+		if err != nil {
+			assert.Contains(t, err.Error(), expectedError)
+			return
+		}
+
+		out, _, err := tx.Transform([]byte(in))
+		if assert.Nil(t, out) && assert.Error(t, err) {
+			assert.Contains(t, err.Error(), expectedError)
+		}
+	}
+
+	check("$apple^", "$1", "hello", "hello", false) // keep as is
+	check("^.*(ell).*$", "Z$1", "hello", "Zell", false)
+	bad("$(", "$1", "hello", "error parsing regexp")
+}
+
+// test script-call transformation
+func TestScriptCall(t *testing.T) {
 	// good case
 	check := func(script []string, in string, expectedOut string, expectedSkip bool) {
 		tx, err := NewScriptCall(script, "")
