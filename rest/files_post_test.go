83a292af5b5bd0c14a85106d3b6510e65acf7f31
Vadim Filatov <default87@gmail.com>
2017-05-17 18:33:03 +0300

    Update /files post tests

--- rest/files_post_test.go
+++ rest/files_post_test.go
@@ -1,11 +1,14 @@
 package rest
 
 import (
+	"encoding/json"
 	"io/ioutil"
 	"net/http"
 	"path/filepath"
 	"testing"
 	"time"
+
+	"os"
 
 	"github.com/stretchr/testify/assert"
 )
@@ -14,6 +17,8 @@ import (
 func TestPostFiles(t *testing.T) {
 	fs := newFake()
 	defer fs.cleanup()
+	hostname, err := os.Hostname()
+	assert.NoError(t, err)
 
 	go func() {
 		err := fs.worker.ListenAndServe()
@@ -25,8 +30,16 @@ func TestPostFiles(t *testing.T) {
 		time.Sleep(100 * time.Millisecond) // wait a bit until server is stopped
 	}()
 
+	type Item struct {
+		Length   int    `json:"length"`
+		Offset   int    `json:"offset"`
+		Path     string `json:"path"`
+		Hostname string `json:"hostname,omitempty"`
+		Error    string `json:"error,omitempty"`
+	}
+
 	// test case
-	check := func(url, accept string, contentType, data string, cancelIn time.Duration, expectedStatus int, expectedErrors ...string) {
+	check := func(url, accept string, contentType, data string, cancelIn time.Duration, expectedStatus int, expectedResponseBody []Item, expectedErrors ...string) {
 		body, status, err := fs.POST(url, accept, contentType, data, cancelIn)
 		if err != nil {
 			for _, msg := range expectedErrors {
@@ -34,13 +47,10 @@ func TestPostFiles(t *testing.T) {
 			}
 		} else {
 			assert.EqualValues(t, expectedStatus, status)
-			for _, msg := range expectedErrors {
-				if expectedStatus == http.StatusOK {
-					assert.JSONEq(t, msg, string(body))
-				} else {
-					assert.Contains(t, string(body), msg)
-				}
-			}
+			var bodyUnmarshalled []Item
+			err = json.Unmarshal(body, &bodyUnmarshalled)
+			assert.NoError(t, err)
+			assert.Equal(t, expectedResponseBody, bodyUnmarshalled)
 		}
 	}
 
@@ -55,26 +65,51 @@ func TestPostFiles(t *testing.T) {
 	all := false // false
 
 	if all {
-		check("/files1", "", "", "hello", 0, http.StatusNotFound, "page not found")
+		check("/files1", "", "", "hello", 0, http.StatusNotFound, nil, "page not found")
 
 		check("/files?dir=foo&file=1.txt", "", "", "hello", 0,
-			http.StatusBadRequest, "unexpected content type")
+			http.StatusBadRequest, nil, "unexpected content type")
 	}
 
 	if all || true {
 		// upload a file
 		check("/files?file=foo/2.txt", "", "application/octet-stream",
-			`hello`, 0, http.StatusOK, `[{"length":5, "offset":0, "path":"foo/2.txt"}]`)
+			`hello`, 0, http.StatusOK,
+			[]Item{
+				Item{
+					Length:   5,
+					Offset:   0,
+					Path:     "foo/2.txt",
+					Hostname: hostname,
+				},
+			},
+		)
 		checkFile("foo/2.txt", `hello`)
 
 		// append a file
 		check("/files?file=foo/2.txt", "", "application/octet-stream",
-			` world`, 0, http.StatusOK, `[{"length":6, "offset":5, "path":"foo/2.txt"}]`)
+			` world`, 0, http.StatusOK,
+			[]Item{
+				Item{
+					Length:   6,
+					Offset:   5,
+					Path:     "foo/2.txt",
+					Hostname: hostname,
+				},
+			})
 		checkFile("foo/2.txt", `hello world`)
 
 		// replace a part of file
 		check("/files?file=foo/2.txt&offset=2", "", "application/octet-stream",
-			`y!!`, 0, http.StatusOK, `[{"length":3, "offset":2, "path":"foo/2.txt"}]`)
+			`y!!`, 0, http.StatusOK,
+			[]Item{
+				Item{
+					Length:   3,
+					Offset:   2,
+					Path:     "foo/2.txt",
+					Hostname: hostname,
+				},
+			})
 		checkFile("foo/2.txt", `hey!! world`)
 	}
 }
